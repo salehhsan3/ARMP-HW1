c95fe963d13b3b56c709ca750decd40ab310f9eb
unknown <salehhsan3@gmail.com>
2024-02-08 17:38:51 +0200

    working on vis graph

--- HW1.py
+++ HW1.py
@@ -6,6 +6,14 @@ from Plotter import Plotter
 from shapely.geometry.polygon import Polygon, LineString
 import math
 
+# 0,0 1,0 1,1 0,1 
+# 10,3 11,3 11,5 11,6 10,5 
+# -5,-5 -3,-5 -4,0 
+# 2,0 3,-1 2,-3 4,-4 3,-5 0,-4 0,-2 
+# 10,0 10,1 11,2 13,1 12,-2 11,-2 
+
+CORRECT_OBSTACLES = True
+
 def combine_points(p1, p2):
     return (p1[0] + p2[0], p1[1] + p2[1])
     
@@ -37,8 +45,13 @@ def get_minkowsky_sum(original_shape: Polygon, r: float) -> Polygon:
     :param r: The radius of the rhombus
     :return: The polygon composed from the Minkowsky sums
     """
+    # assumptions:
+    #   we'll denote vertices of the robot - v_i. and vertices of the obstacle - w_j
+    #   1) v_1 and w_1 have the smallest y-coordinates in their respective lists
+    #   2) the vertices are arranged counter clock-wise.
     robot_coords = [(0,-r), (r,0),(0,r),(-r,0)]         #P
     obst_coords = original_shape.exterior.coords[:-1]   #Polygon repeats the last step
+    
     mink_sum = []
 
     i, j = 0, 0
@@ -62,7 +75,34 @@ def get_visibility_graph(obstacles: List[Polygon], source=None, dest=None) -> Li
     :param dest: The destination of the query. None for part 1.
     :return: A list of LineStrings holding the edges of the visibility graph
     """
-    return [(0,0),(1,1)]
+    visibility_edges = []
+    # nodes = [obstacle.centroid for obstacle in obstacles]
+    nodes = [vertex for obstacle in obstacles for vertex in obstacle.exterior.coords[:-1]]
+    for p in nodes:
+        for q in nodes:
+            if p == q:
+                continue
+            line = LineString([p, q])
+            intersection_points = []
+            intersection_lines = []
+            for obstacle in obstacles:
+                intersection = line.intersection(obstacle)
+                if intersection.is_empty:
+                    continue
+                elif intersection.geom_type == 'Point':
+                    intersection_points.append(intersection)
+                elif intersection.geom_type == 'MultiPoint':
+                    intersection_points.extend(intersection)
+                elif intersection.geom_type == 'LineString':
+                    intersection_lines.append(intersection)
+                else:
+                    print("Error, unhandled intersection type!")
+                    exit(0xdeadbeef)
+            if all(point not in intersection_points for point in [p, q]) and \
+               all(line not in intersection_lines for line in [line, line.reverse()]):
+                visibility_edges.append(line)
+    
+    return visibility_edges
 
 
 def is_valid_file(parser, arg):
@@ -76,35 +116,7 @@ def get_points_and_dist(line):
     source = tuple(map(float, source.split(',')))
     return source, dist
 
-# def my_main():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("Robot", help="A file that holds the starting position of the robot, and the distance from the center of the robot to any of its vertices")
-#     parser.add_argument("Obstacles", help="A file that contains the obstacles in the map")
-#     parser.add_argument("Query", help="A file that contains the ending position for the robot.")
-#     args = parser.parse_args()
-#     obstacles = args.Obstacles
-#     robot = args.Robot
-#     query = args.Query
-#     workspace_obstacles = []
-#     with open(obstacles, 'r') as f:
-#         for line in f.readlines():
-#             ob_vertices = line.split(' ')
-#             if ',' not in ob_vertices:
-#                 ob_vertices = ob_vertices[:-1]
-#             points = [tuple(map(float, t.split(','))) for t in ob_vertices]
-#             workspace_obstacles.append(Polygon(points))
-#     with open(robot, 'r') as f:
-#         source, dist = get_points_and_dist(f.readline())
-#     c_space_obstacles = [get_minkowsky_sum(p, dist) for p in workspace_obstacles]
-#     plotter1 = Plotter()
-#     plotter1.add_obstacles(workspace_obstacles)
-#     plotter1.add_c_space_obstacles(c_space_obstacles)
-#     plotter1.add_robot(source, dist)
-
-#     plotter1.show_graph()
-
 if __name__ == '__main__':
-    # my_main()
     parser = argparse.ArgumentParser()
     parser.add_argument("Robot", help="A file that holds the starting position of the robot, and the distance from the center of the robot to any of its vertices")
     parser.add_argument("Obstacles", help="A file that contains the obstacles in the map")
@@ -130,25 +142,23 @@ if __name__ == '__main__':
     # step 1:
     c_space_obstacles = [get_minkowsky_sum(p, dist) for p in workspace_obstacles]
     plotter1 = Plotter()
-    print("done computing")
     plotter1.add_obstacles(workspace_obstacles)
     plotter1.add_c_space_obstacles(c_space_obstacles)
     plotter1.add_robot(source, dist)
 
     plotter1.show_graph()
+    # step 2:
+    print("get graph")
+    lines = get_visibility_graph(c_space_obstacles)
+    plotter2 = Plotter()
+
+    plotter2.add_obstacles(workspace_obstacles)
+    plotter2.add_c_space_obstacles(c_space_obstacles)
+    plotter2.add_visibility_graph(lines)
+    plotter2.add_robot(source, dist)
+
+    plotter2.show_graph()
     print("done showing")
-    # # step 2:
-
-    # lines = get_visibility_graph(c_space_obstacles)
-    # plotter2 = Plotter()
-
-    # plotter2.add_obstacles(workspace_obstacles)
-    # plotter2.add_c_space_obstacles(c_space_obstacles)
-    # plotter2.add_visibility_graph(lines)
-    # plotter2.add_robot(source, dist)
-
-    # plotter2.show_graph()
-
     # # step 3:
     # with open(query, 'r') as f:
     #     dest = tuple(map(float, f.readline().split(',')))
